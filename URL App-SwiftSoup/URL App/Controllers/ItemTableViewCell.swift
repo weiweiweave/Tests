c803fcbf10734f2ad24a02250142dbbd85cb8ac4
weiweiweave <lockheartbee@gmail.com>
2020-05-31 12:29:20 +0800

    Create ItemTableViewCell.swift

---
+++ URL App-SwiftSoup/URL App/Controllers/ItemTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemTableViewCell.swift
+//  URL App
+//
+//  Created by Weiping Tok on 22/5/20.
+//  Copyright © 2020 Weiping Tok. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet var titleLabel:UILabel!
+    @IBOutlet var urlLabel:UILabel!
+
+}
