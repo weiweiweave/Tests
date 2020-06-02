8e3e9bbf4b39331b631020940dc1f76a34ddc24a
weiweiweave <weiweiweave@hotmail.com>
2020-06-02 12:44:39 +0800

    Add files via upload

---
+++ RoomBooking/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//
+//  Created by Weiping Tok on 29/3/20.
+//  Copyright © 2020 Bluemoon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct K {
+    //static makes it a type property
+    static let sortSegue = "goToSort"
+    static let scanSegue = "goToScan"
+    static let detailSegue = "goToDetail"
+    static let RoomCellReuseIdentifier = "RoomCell"
+    
+    
+    
+    
+}
+
+
+
