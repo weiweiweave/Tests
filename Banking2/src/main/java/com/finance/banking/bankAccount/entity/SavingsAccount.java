108091062cf8018349196201c1f24eb95851340f
Weiping <weiweiweave@hotmail.com>
2023-07-06 09:38:07 +0800

    Updates

---
+++ Banking2/src/main/java/com/finance/banking/bankAccount/entity/SavingsAccount.java
@@ -0,0 +1,15 @@
+package com.finance.banking.bankAccount.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SavingsAccount extends BankAccount {
+
+    private Long annualInterestRate;
+
+    public void depositMonthlyInterest (Long amount) {
+        //
+    }
+}
