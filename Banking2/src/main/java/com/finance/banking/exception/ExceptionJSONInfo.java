8acd28bc1d374f15c5b8ef9dbb44b741b353037c
Weiping <weiweiweave@hotmail.com>
2023-07-12 18:03:25 +0800

    Updates

--- Banking2/src/main/java/com/finance/banking/exception/ExceptionJSONInfo.java
+++ Banking2/src/main/java/com/finance/banking/exception/ExceptionJSONInfo.java
@@ -1,20 +1,13 @@
 package com.finance.banking.exception;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ExceptionJSONInfo {
 
     private String url;
     private String message;
-
-    public String getUrl() {
-        return url;
-    }
-    public void setUrl(String url) {
-        this.url = url;
-    }
-    public String getMessage() {
-        return message;
-    }
-    public void setMessage(String message) {
-        this.message = message;
-    }
+    private String cause;
 }
--- Banking2/src/main/java/com/finance/banking/exception/GlobalExceptionHandler.java
+++ Banking2/src/main/java/com/finance/banking/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.finance.banking.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,10 +17,22 @@ public class GlobalExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public String handleDataIntegrityViolation(HttpServletRequest request, Exception exception) {
+    public @ResponseBody ExceptionJSONInfo handleDataIntegrityViolation(HttpServletRequest request, Exception ex) {
         //ResponseEntity<Object>
         //return new ResponseEntity<>("DataIntegrityViolationException", HttpStatus.INTERNAL_SERVER_ERROR);
-        logger.trace("SQLException Occured:: URL="+request.getRequestURL());
-        return "database_error";
+        logger.trace("DataIntegrityViolationException Occured:: Cause="+ ExceptionUtils.getRootCauseMessage(ex));
+        //return "database_error";
+
+        logger.trace("DataIntegrityViolationException Occured:: Cause="+ ExceptionUtils.getStackTrace(ex));
+
+
+
+
+        ExceptionJSONInfo response = new ExceptionJSONInfo();
+        response.setUrl(request.getRequestURL().toString());
+        //response.setMessage(ex.getStackTrace().toString());
+        response.setCause(ex.getLocalizedMessage());
+
+        return response;
     }
 }
