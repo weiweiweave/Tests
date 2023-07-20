55064d072debc05913ecd9b38c54207e27d9ab67
Weiping <weiweiweave@hotmail.com>
2023-07-20 18:06:31 +0800

    Updates

---
+++ Banking2/src/main/java/com/digital/ace/java/banking/customer/dto/CreateCustomerRequest.java
@@ -0,0 +1,32 @@
+package com.digital.ace.java.banking.customer.dto;
+
+public class CreateCustomerRequest {
+
+    private String staffIdWhoKeyIn;
+
+    private String company;
+
+    private String fundSource;
+
+    private String address;
+
+    private String city;
+
+    private String joinedDate;
+
+    private String nric;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String sex;
+
+    private String emailAddress;
+
+    private String phone;
+
+    private String dateOfBirth;
+
+    private String jobTitle;
+}
